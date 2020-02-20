2bd26f00737206948169ddb4d1d5d1fafa9196a3
sr1jan <srofficialsingh@gmail.com>
2020-02-21 02:29:14 +0530

    latest

--- sol_1.py
+++ sol_1.py
@@ -1,7 +1,6 @@
 if __name__ == '__main__':
     info = list(map(int, input().split()))
     B, L, D = info[0], info[1], info[2]
-    print(D)
 
     B_scores = list(map(int, input().split()))
 
@@ -27,25 +26,31 @@ if __name__ == '__main__':
     L_order = [l for l in range(L)]
     L_order = sorted(L_order, key=lambda x: L_info[x][2], reverse=True)
 
-    # seen_b = set()
-    # for l in L_order:
-    #     bks = L_info[l][1]
-    #     if(len(bks)) is 0: continue
-    #     bks = list(set(bks)-seen_b)
 
-    #     for b in bks: seen_b.add(b)
-    #     L_info[l][1] = bks
+    seen_b = set()
+    for l in L_order:
+        bks = L_info[l][1]
+        if(len(bks)) is 0: continue
+        bks = list(set(bks)-seen_b)
+        b_sum = 0
+        for b in bks:
+            b_sum += B_scores[b]
 
+        for b in bks: seen_b.add(b)
+        L_info[l][1] = bks
+        L_info[l][2] = b_sum
 
-    # n_l = [c for c in L_order if len(L_info[c][1]) is not 0]
-    # print(len(n_l))
-    # for l in L_order:
-    #     n_b = len(L_info[l][1])
-    #     if n_b is 0: continue
-    #     print(l, n_b)
-    #     for b in L_info[l][1]:
-    #         print(b, end=' ')
-    #     print()
+    L_order = sorted(L_order, key=lambda x: L_info[x][2], reverse=True)
+
+    n_l = [c for c in L_order if len(L_info[c][1]) is not 0]
+    print(len(n_l))
+    for l in L_order:
+        n_b = len(L_info[l][1])
+        if n_b is 0: continue
+        print(l, n_b)
+        for b in L_info[l][1]:
+            print(b, end=' ')
+        print()
 
 
 
