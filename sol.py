ee0dcece11774411ef6ee85d73e3c2941e3e86fb
sr1jan <srofficialsingh@gmail.com>
2020-02-21 00:06:10 +0530

    input files

--- sol.py
+++ sol.py
@@ -1,2 +1,24 @@
 if __name__ == '__main__':
-    print('#Hashcode2020')
+    info = list(map(int, input().split()))
+    B, L, D = info[0], info[1], info[2]
+
+    B_scores = list(map(int, input().split()))
+
+    L_info = {}
+    for i in range(L):
+        l = list(map(int, input().split()))
+        L_info[i] = [[l[0], l[1], l[2]], [], []]
+        books = list(map(int, input().split()))
+        books = sorted(books, key=lambda x: B_scores[x])
+        b_sum = 0
+        for b in books:
+            b_sum += B_scores[b]
+        L_info[i][1] = books
+        L_info[i][2] = b_sum
+
+    print(B, L, D)
+    print(B_scores)
+    print(L_info)
+
+
+
